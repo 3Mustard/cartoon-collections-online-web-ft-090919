078351168bb6d355dc2d4a67fd557675a9fd2b12
Matthew C <m23.cullen@gmail.com>
2019-09-11 17:23:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.include? do |word| 
-    word.length > 4 
-  end
+  array.each do |name|
+    
 end
 
 def find_the_cheese# code an argument here
