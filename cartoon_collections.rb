f779559c8f50ca7d8f5b5d55c7c7edff99bb8a79
Matthew C <m23.cullen@gmail.com>
2019-09-11 17:17:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,8 @@
 def roll_call_dwarves(array)
   array.each_with_index do |name,index|
     index += 1 
-    puts ""
+    puts "#{index}. #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
