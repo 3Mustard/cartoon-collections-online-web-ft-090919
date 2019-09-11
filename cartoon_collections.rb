ca700a9eec5d4e890bb7c2a76e9c6ad8548aa542
Matthew C <m23.cullen@gmail.com>
2019-09-11 17:33:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? do |i| i > 4 end
+  array.any? do |i| 
+    i.length > 4 
+  end
 end
 
 def find_the_cheese# code an argument here
